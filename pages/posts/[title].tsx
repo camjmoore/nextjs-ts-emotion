ad32ab4c604288add22cdea529108a2085521803
octanym <cmrnmr@gmail.com>
2022-04-27 11:34:10 -0400

    basic next router query params, absolute imports in tsconfig

--- pages/posts/[title].tsx
+++ pages/posts/[title].tsx
@@ -2,9 +2,14 @@ import { Article } from '../../components/Article';
 import { useRouter } from "next/router";
 
 export default function Post() {
+
+  const router = useRouter();
+
+  const { title } = router.query;
+
   return (
     <Article>
-      <h1>Sushumna</h1>
+      <h1>{title}</h1>
       <p>From East to West and back again, accord to the azimuth such that you may return to the horizon of your first season; Eternal Spring</p>
     </Article>
   );
