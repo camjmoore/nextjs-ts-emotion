9893c2e6ee10d44b7961ae2d988bfd7f6f0ae7b3
octanym <cmrnmr@gmail.com>
2022-04-26 20:56:00 -0400

    routing - components, about, posts

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,10 @@
+import { Article } from '../components/Article';
+
 export default function About() {
   return (
-    <div>
+    <Article>
       <h1>The Acrylic Whale Grave</h1>
       <p>This is the biotic luminary, which whosoever he may be, will glorify the cosmic sands that consume creation</p>
-    </div>
+    </Article>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
-import styled from "@emotion/styled"
+import styled from '@emotion/styled'
 
 const Container = styled.div`
   padding: 0 2rem;
