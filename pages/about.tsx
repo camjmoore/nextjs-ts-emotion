21f00dd31c753514b0d1f99d539a5e55f589972d
octanym <cmrnmr@gmail.com>
2022-04-27 11:36:13 -0400

    update about and title files with absolute imports

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,10 @@
-import { Article } from '../components/Article';
+import { Article } from '@components/Article';
 
 export default function About() {
   return (
     <Article>
       <h1>The Acrylic Whale Grave</h1>
-      <p>This is the biotic luminary, which whosoever he may be, will glorify the cosmic sands that consume creation</p>
+      <p>This is the biotic luminary, which, whosoever may he be, will glorify the cosmic sands that consume creation</p>
     </Article>
   );
 }
